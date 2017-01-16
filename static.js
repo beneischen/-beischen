d531706a31e952821a113e9aaa6db891137b691e
beneischen <beischen@windham.local>
2017-01-16 17:57:10 -0500

    changed font switching

--- static.js
+++ static.js
@@ -6,9 +6,12 @@ window.onload = function() {
 
     var thresholdWidth = 1000;
 
+    var bigFont = false;
+
     if (pageWidth <= thresholdWidth){
     	// change the width of all inner divs to 100%
     	$(".inner-div").css("width", "90%");
+    	bigFont = true;
     }
     
     $( window ).bind("resize", function(){
@@ -16,12 +19,28 @@ window.onload = function() {
 
 	    if (pageWidth > thresholdWidth){
 	    	// Change the width of the div when the pageWidth is greater than threshold
-	    	$(".inner-div").css("width", "35%" );
+	    	$(".inner-div").css("width", "45%" );
+
+	    	if (bigFont == true) {
+		    	var currentFontSize = $(".inner-div").css('font-size');
+			    var currentFontSizeNum = parseFloat(currentFontSize, 10);
+			    var newFontSize = currentFontSizeNum*0.8;
+			    $(".inner-div").css('font-size', newFontSize);
+			    bigFont = false;
+		    }
 	    }
 
 	    else if (pageWidth <= thresholdWidth) {
 	    	// Change width when pageWidth is less than or equal to threshold pixels
-	    	$(".inner-div").css("width", "90%" );	
+	    	$(".inner-div").css("width", "90%" );
+
+	    	if (bigFont == false) {
+		    	var currentFontSize = $(".inner-div").css('font-size');
+			    var currentFontSizeNum = parseFloat(currentFontSize, 10);
+			    var newFontSize = currentFontSizeNum*1.2;
+			    $(".inner-div").css('font-size', newFontSize);
+			    bigFont = true;
+		    }
 	    }
 
 	});
